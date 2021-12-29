3e63f615446301094ab7f06a969fc08765908540
Ousseine <ousseinetech@gmail.com>
2021-12-29 16:06:05 +0100

    add files ignoring

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+import 'bootstrap';
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
+
+import 'jquery';
+import $ from 'jquery';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
